3d2ca2c4edaeb381a2d76ecbb18d1a92ea29b2d8
Orhan CINAR <orhancinar35@gmail.com>
2019-01-02 02:35:28 +0100

    added Alarm Trigger

---
+++ AlarmTrigger.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.5.0;
+
+interface AlarmWakeUp{
+    function callback(bytes calldata _data) external;
+}
+
+contract AlarmService{
+    
+    struct TimeEvent {
+        address addr;
+        bytes data;
+    }
+    
+    mapping(uint => TimeEvent[]) private _events;
+    
+    function set(uint _time) public returns (bool){
+        TimeEvent memory _timeEvent;
+        _timeEvent.addr = msg.sender;
+        _timeEvent.data = msg.data;
+        
+        
+        _events[_time].push(_timeEvent);
+    }
+    
+    function call(uint _time) public {
+        TimeEvent[] memory timeEvents = _events[_time];
+        
+        for(uint i = 0; i < timeEvents.length; i++){
+            AlarmWakeUp(timeEvents[i].addr).callback(timeEvents[i].data);
+            
+        }
+    }
+}
+
+contract AlarmTrigger is AlarmWakeUp{
+    
+    AlarmService private _alarmService;
+    
+    constructor() internal { 
+        _alarmService = new AlarmService();
+    }
+    
+    function callback() public {
+        
+    }
+    
+    function setAlarm() public {
+        _alarmService.set(block.timestamp+60);
+    }
+}
