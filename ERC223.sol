39bdce8f3884d919aedd49d81290a506f0f2b4ff
Orhan CINAR <orhancinar35@gmail.com>
2019-01-14 02:49:02 +0100

    update erc223

--- ERC223.sol
+++ ERC223.sol
@@ -1,4 +1,4 @@
 interface ERC223{
-     function transfer(address to, uint value) public returns (bool ok);
+     function transfer(address to, uint value) external returns (bool ok);
      event Transfer(address indexed from, address indexed to, uint value, bytes indexed data);
 }
